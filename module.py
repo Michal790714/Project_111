da6ef95d86708d4ee24de57a5bdcb92c9da23f15
Michal790714 <86305140+Michal790714@users.noreply.github.com>
2024-12-11 16:46:13 +0100

    Add files via upload

---
+++ module.py
@@ -0,0 +1,8 @@
+print("Zaciname gitovat")
+list1 = [1, 2, 3, 4, 5]
+dictionary = {"one": 1}
+print("Hello World")
+print("Procedli jsme zmeny")
+print("Pridal jsem dalsi print")
+
+print("Projekt 1")
